d031c7a073c788b7978767c8c5b7d02563d29414
Dodi Suprayogi <52416894+tugelsikile@users.noreply.github.com>
2023-06-30 07:33:44 +0700

    Update fix-directory.sh

--- fix-directory.sh
+++ fix-directory.sh
@@ -1,12 +1,58 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "cmodding 755 of $SCRIPT_DIR"
-chmod -R 755 "$SCRIPT_DIR"
-echo "chmodding 777 of bootstrap dir"
+
+ROOT_DIR="$SCRIPT_DIR"
+echo "checking root directory permissions at $ROOT_DIR"
+if [ "$(stat -c '%a' "$ROOT_DIR")" == "755" ]
+then
+    echo "changing permissions of $ROOT_DIR"
+    chmod -R 755 "$ROOT_DIR"
+else
+    echo "already check permissions. nothing todo"
+fi
+PUBLIC_DIR="$SCRIPT_DIR/public"
+echo "checking public directory permissions at $PUBLIC_DIR"
+if [ "$(stat -c '%a' "$PUBLIC_DIR")" == "755" ]
+then
+    echo "changing permissions of $PUBLIC_DIR"
+    chmod -R 755 "$PUBLIC_DIR"
+else
+    echo "already check permissions. nothing todo"
+fi
+
 BOOTSTRAP_DIR="$SCRIPT_DIR/bootstrap"
-chmod -R 777 "$BOOTSTRAP_DIR"
-echo "chmodding 777 of storage dir"
+echo "checking bootstrap directory permissions at $BOOTSTRAP_DIR"
+if [ "$(stat -c '%a' "$BOOTSTRAP_DIR")" == "755" ]
+then
+    echo "changing permissions of $BOOTSTRAP_DIR"
+    chmod -R 777 "$BOOTSTRAP_DIR"
+else
+    echo "already check permissions. nothing todo"
+fi
+
 STORAGE_DIR="$SCRIPT_DIR/storage"
-chmod -R 777 "$STORAGE_DIR"
+echo "checking storage directory permissions at $STORAGE_DIR"
+if [ "$(stat -c '%a' "$STORAGE_DIR")" == "755" ]
+then
+    echo "changing permissions of $STORAGE_DIR"
+    chmod -R 777 "$STORAGE_DIR"
+else
+    echo "already check permissions. nothing todo"
+fi
+
+PUBLIC_STORAGE="$SCRIPT_DIR/public/storage"
+echo "checking symlink storage in #storage_link at $PUBLIC_STORAGE"
+if [ -h "$PUBLIC_STORAGE" ]; then
+    echo "remove storage link first"
+    rm "$PUBLIC_STORAGE"
+    echo "linking storage"
+    php artisan storage:link
+    echo "finish linking storage"
+    echo "chmod 777 public storage dir"
+    chmod -R 777 "$PUBLIC_STORAGE"
+else
+    echo "linking storage"
+    php artisan storage:link
+fi
 
 vendor_dir="$SCRIPT_DIR/vendor"
 echo "checking if vendor exists"
@@ -16,38 +62,3 @@ else
     echo "running composer install"
     composer install
 fi
-
-storage_link="$SCRIPT_DIR/public/storage"
-echo "checking symlink storage in #storage_link"
-if [ -h "$storage_link" ]; then
-    echo "remove storage link first"
-    rm "$storage_link"
-    echo "linking storage"
-    php artisan storage:link
-    echo "finish linking storage"
-else
-    echo "linking storage"
-    php artisan storage:link
-fi
-
-echo "checking root directory permissions"
-root_dir="$SCRIPT_DIR"
-if [ "$(stat -c '%a' "$root_dir")" == "755" ]
-then
-    echo "changing permissions of $root_dir"
-    chmod -R 755 "$root_dir"
-  # something
-else
-    echo "already check permissions. nothing todo"
-fi
-public_dir="$SCRIPT_DIR/public"
-echo "checking public directory permissions"
-if [ "$(stat -c '%a' "$public_dir")" == "755" ]
-then
-    echo "changing permissions of $public_dir"
-    chmod -R 755 "$public_dir"
-else
-    echo "already check permissions. nothing todo"
-fi
-
-chmod -R 755 "$SCRIPT_DIR"
